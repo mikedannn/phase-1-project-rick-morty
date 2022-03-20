2ede3538c336ba49bc74fe786f64672be995ede7
Michael <michael.daniels1013@gmail.com>
2022-03-20 18:37:50 -0500

    Rendered 1 Character to page

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // hello this is the project
+
+const URL = 'https://rickandmortyapi.com/api/character/1'
+const card = document.getElementById('card');
+const gallery = document.getElementById('character_gallery');
+const selectedImage = document.getElementById('selectedImage');
+const btn = document.getElementById('characterBtn');
+const characterImages = [];
+
+const renderCharacter = (character) => {
+    let characterCard = document.createElement('div');
+    
+
+    characterCard.innerHTML =
+
+    `<div class="card">
+        <img src="${character.image}" alt="Avatar" style="width:100%">
+        <div class="container">
+            <h4><b>${character.name}</b></h4> 
+            <p>SPECIES: ${character.species}</p> 
+            <p>GENDER: ${character.gender}</p> 
+            <p>STATUS: ${character.status}</p> 
+        </div>
+    </div>`
+
+    card.appendChild(characterCard);
+
+
+
+
+    // characterCard.innerHTML = 
+    // `
+    //     <img id="${character.id}" src="${character.image}" />
+    //     <li>NAME: ${character.name}</li>
+    //     <li>SPECIES: ${character.species}</li>
+    //     <li>GENDER: ${character.gender}</li>
+    //     <li>STATUS: ${character.status}</li>
+    // `;
+
+    // gallery.appendChild(characterCard);
+}
+
+btn.addEventListener('click', () => {
+    fetch(URL)
+        .then(res => res.json())
+        .then(character => renderCharacter(character))
+        // .then(character => renderCharacter())
+        // document.getElementById('character_gallery').innerHTML = characterHTML
+})
+
+
